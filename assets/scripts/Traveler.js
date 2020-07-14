4b22528bdf2e1d070b28fa15f7cb9b7a98cc1bcd
Douglas Wilson <dougwilson0000@yahoo.com>
2020-07-14 09:44:47 -0400

    some minor fixes

--- assets/scripts/Traveler.js
+++ assets/scripts/Traveler.js
@@ -1,15 +1,32 @@
 class Traveler {
-    constructor(name) {
+    constructor(name, gender) {
         this.name = name
         this.food = 1
         this.isHealthy = true
+        this.gender = gender
     }
 
     hunt() {
+        this.isHealthy = true
        return this.food = this.food + 2
     }
     eat() {
-       return (this.food > 0) ? this.food = this.food - 1 : this.isHealthy = false
+        if(this.food > 1) {
+            this.food = this.food -1
+            this.isHealthy = true
+            return
+        }
+    //    (this.food > 1) ? this.food = this.food - 1
+        if(this.food === 1) {
+            this.food = this.food - 1
+            this.isHealthy = false
+            return
+        }
+    //    (this.food === 1) ? this.food = this.food - 1 this.isHealthy = false
+        if(this.food === 0) {
+            this.isHealthy = false
+            return
+        }
     }
 
 } 
--- assets/scripts/Wagon.js
+++ assets/scripts/Wagon.js
@@ -1,14 +1,57 @@
+const main = document.querySelector('main')
+
 class Wagon {
     constructor(capacity) {
         this.capacity = capacity
         this.passengers = []
+        
+        main.style.maxWidth = '900px'
+        main.style.margin = 'auto'
+        let wagonDom = document.createElement('div')
+        wagonDom.append(`This wagon can hold ${this.capacity} passengers.`)
+        wagonDom.style.textAlign = 'center'
+        let wagonImg = document.createElement('img')
+        wagonImg.src = './assets/images/wagon.jpg'
+        wagonImg.style.textAlign = 'center'
+        wagonImg.style.display = 'block'
+        wagonImg.style.margin = 'auto'
+        main.append(wagonDom, wagonImg)
+        
 
     }
     getAvailableSeatCount() {
         return this.capacity - this.passengers.length
     }
     join(traveler) {
-        if(this.getAvailableSeatCount() > 0) return this.passengers.push(traveler)
+        let box = document.createElement('div')
+        box.className = 'people'
+        let breaky = document.createElement('div')
+        let breaky1 = document.createElement('br')
+        let breaky2 = document.createElement('br')
+        let breaky3 = document.createElement('br')
+        breaky.className = 'break'
+        main.append(box) 
+        let boyImage = document.createElement('img')
+        let girlImage = document.createElement('img')
+        boyImage.src = './assets/images/boy.png'
+        girlImage.src = './assets/images/girl.png'
+        
+        if(this.getAvailableSeatCount() > 0 && traveler.gender === 'boy') {
+        box.append(boyImage)
+        breaky.append( 'Name: ', traveler.name,breaky1, 'Food: ', traveler.food, breaky2, 'Health: ', traveler.isHealthy, breaky3, 'Gender: ', traveler.gender)
+        box.append(breaky)
+        return this.passengers.push(traveler)
+        } 
+        if(this.getAvailableSeatCount() > 0 && traveler.gender === 'girl') {
+        box.append(girlImage)
+        breaky.append( 'Name: ', traveler.name,breaky1, 'Food: ', traveler.food, breaky2, 'Health: ', traveler.isHealthy, breaky3, 'Gender: ', traveler.gender)
+        box.append(breaky)
+        return this.passengers.push(traveler)  }
+        
+        
+
+        
+        
     }
     shouldQuarantine() {
         const quarantine = this.passengers.some(quaran => quaran.isHealthy === false ) 
@@ -16,8 +59,10 @@ class Wagon {
         return quarantine
     }
     totalFood() {
-        const foodAdded = this.passengers.reduce((a) => a.food)
-        return foodAdded
+        
+        const foodAdded = this.passengers.reduce((sum, cur) => sum + cur.food, 0 )
+        
+        return foodAdded                                       
     }
 
 }
